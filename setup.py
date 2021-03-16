c63f5165ebb903381ad2b36c9189f4e51626a716
dvp <dmitri_portnov@yahoo.com>
2021-03-16 14:55:23 +0300

    style: warnings in setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,35 @@
 import setuptools
-# from glob import glob
 
-# Get the version from wwgvr/__init__.py
+# Get the version from wwgvr/__version__.py
 fd = {}
-with open('./iww_gvr/__version__.py', 'r') as f:
+with open("./iww_gvr/__version__.py", "r") as f:
     exec(f.read(), fd)
-    version = fd['__version__']
+    version = fd["__version__"]
+# TODO dvp: why do not just read the file and parse the line with "__version__"? Or even better switch to pyproject.toml
+
 
 setuptools.setup(
     name="iww_gvr",
-    version=version, # __version__
+    version=version,
     author="Marco Fabbri, Alvaro Cubi",
     author_email="marco.fabbri@f4e.europa.eu",
     description="Weight window Manipulator & Global Variance Reduction Tool",
-    # url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
-#    data_files=[glob('doc/*'),'testing'
-#    ],
-    include_package_data=True,
+    include_package_data=True,  # TODO dvp: is to be False actually
     install_requires=[
-          "numpy >= 1.14.3","pyevtk>=1.1.1","matplotlib==3.3.3","vtk>=8.1.2","scipy>=1.1.0","tqdm>=4.35.0","pillow",
-      ],
+        "numpy >= 1.14.3",
+        "pyevtk>=1.1.1",
+        "matplotlib==3.3.3",
+        "vtk>=8.1.2",
+        "scipy>=1.1.0",
+        "tqdm>=4.35.0",
+        "pillow",
+        "PyQt5",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={'console_scripts': ['iww_gvr = iww_gvr.main:main']},
+    entry_points={"console_scripts": ["iww_gvr = iww_gvr.main:main"]},
 )
