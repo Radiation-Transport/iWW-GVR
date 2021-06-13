b195691d872f72824f966a138d489b09aec2d5fd
dvp <dmitri_portnov@yahoo.com>
2021-06-13 12:34:39 +0300

    build: add files to install dependencies and run tests

--- setup.py
+++ setup.py
@@ -1,30 +1,62 @@
 import setuptools
-# from glob import glob
+import sys
 
-# Get the version from wwgvr/__init__.py
-fd = {}
-with open('./iww_gvr/__version__.py', 'r') as f:
-    exec(f.read(), fd)
-    version = fd['__version__']
+# noinspection PyPep8Naming
+from setuptools.command.test import test as TestCommand
+
+
+# See recommendations in https://docs.pytest.org/en/latest/goodpractices.html
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+def get_version() -> str:
+    fd = {}
+    with open("./iww_gvr/__version__.py", "r") as f:
+        exec(f.read(), fd)
+        return fd["__version__"]
+
+
+version = get_version()
+
 
 setuptools.setup(
     name="iww_gvr",
-    version=version, # __version__
+    version=version,
     author="Marco Fabbri, Alvaro Cubi",
     author_email="marco.fabbri@f4e.europa.eu",
     description="Weight window Manipulator & Global Variance Reduction Tool",
-    # url="https://github.com/pypa/sampleproject",
+    url="git@github.com:Radiation-Transport/iWW-GVR.git",
     packages=setuptools.find_packages(),
-#    data_files=[glob('doc/*'),'testing'
-#    ],
     include_package_data=True,
     install_requires=[
-          "numpy >= 1.14.3","pyevtk>=1.1.1","matplotlib>=2.2.2","vtk>=8.1.2","scipy>=1.1.0","tqdm>=4.35.0","pillow",
-      ],
+        "numpy >= 1.14.3",
+        "pyevtk>=1.1.1",
+        "matplotlib>=2.2.2",
+        "vtk>=8.1.2",
+        "scipy>=1.1.0",
+        "tqdm>=4.35.0",
+        "pillow",
+    ],
+    tests_require=["pytest", "pytest-cov>=2.3.1"],
+    cmdclass={"test": PyTest},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={'console_scripts': ['iww_gvr = iww_gvr.main:main']},
+    entry_points={"console_scripts": ["iww_gvr = iww_gvr.main:main"]},
 )
