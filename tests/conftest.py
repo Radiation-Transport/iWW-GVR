f059452a3915f7a9462b4a2afa1e7cb61bb589ed
Radiation-Transport <48320561+Radiation-Transport@users.noreply.github.com>
2021-10-26 10:08:48 +0200

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-from _pytest.config import Config
-
-markers = [
-    "slow: marks tests as slow (deselect with -m 'not slow')",
-]
-
-def pytest_configure(config: Config) -> None:
-    for m in markers:
-        config.addinivalue_line("markers", m)
